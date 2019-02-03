3d3aaf5889d0b209a3c3eed0e1c61582f3c4d659
Jarrett Ethan Singian <jarsingian@gmail.com>
2019-02-03 21:35:44 +0800

    Added a ParserAbstract class for csv Reading

---
+++ Intellij/src/designchallenge1/ParserAbstract.java
@@ -0,0 +1,13 @@
+package designchallenge1;
+
+import java.util.ArrayList;
+
+public abstract class ParserAbstract {
+
+    ArrayList<String> content;
+
+    abstract void loadFile ();
+
+    abstract void readFile ();
+
+}
